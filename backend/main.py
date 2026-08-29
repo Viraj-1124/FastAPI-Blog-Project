8460cf4abc989ba1d492b42202c07ab09dbcc02d
Rushi <rushikeshs200624@gmail.com>
2026-08-29 20:30:39 +0530

    created create blog and user API

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from core.config import settings
 from db.session import engine
+from apis.base import api_router
+
+def include_routers(app):
+    app.include_router(api_router)
 
 def start_app():
     app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    include_routers(app)
     return app
 
 app = start_app()
